b5245d98cfe53ce9fab8d8b4d0d1a9173edeb5db
lightmaher <lightmaher3@gmail.com>
2022-02-10 22:14:57 +0200

    model change

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    statuses = (
+        ( 'locked' , 'locked'),
+        ( 'unlocked' , 'unlocked')
+    )
+    roles = (
+        ( 'user' , 'user'),
+        ( 'admin' , 'admin')
+           )
+    user_name = models.CharField(max_length=20)
+    user_email = models.EmailField(max_length=40)
+    user_password = models.CharField(max_length=30)
+    user_cr_date = models.DateTimeField(auto_now_add=True)
+    user_status = models.CharField(max_length=20 , choices=statuses)
+    user_role = models.CharField(max_length=20 , choices=roles)
+    def __str__(self) :
+        return self.user_name
+    
+
+
+
