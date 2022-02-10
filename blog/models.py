953430371afe07b4e4f897367511dfd0b74ed09f
Tarek Eldebsy <TarekEldebsy94>
2022-02-10 22:16:02 +0200

    adding models category, subscription

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cat_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.cat_name
+
+class Subscriptions(models.Model):
+    cat_id = models.ForeignKey(Category)
+
+
+
