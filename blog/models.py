87962102d3d32aa2093a5044595352e8ad5a5c92
salma25128 <salmabasuony3@gmail.com>
2022-02-11 20:12:58 +0200

    keys

--- blog/models.py
+++ blog/models.py
@@ -75,3 +75,28 @@ class Tags(models.Model):
 
     def __str__(self):
         return self.tag_name
+
+#create comment.s 
+
+class Comment(models.Models):
+    cmnt_cr_date = models.DateTimeField(auto_now_add=True)
+    cmnt_content = models.CharField(max_length=200, null=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)#fk-post
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)#fk-user
+   
+   # def __str__(self):
+      #return self.comment  
+
+#create reactions.s
+class Reaction(models.Models):
+
+    roles = (
+        ('like' , 'like'),
+        ('dislike' , 'dislike')
+     )
+    reaction = models.charField(choices=roles)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)#fk-post
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)#fk-user
+    
+    #def __str__(self):
+      #return self.reaction
