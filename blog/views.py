a9b43606587d6d80f7f2f755406d4c4917a40c86
yasmina <y.mohamed6399@gmail.com>
2022-02-13 15:15:10 +0200

    navbar templete

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blogApp/home.html')
+def navbar(request):
+    return render(request, 'blogApp/navbar.html')
+def login(request):
+    return render(request, 'blogApp/login.html')
+def signup(request):
+    return render(request, 'blogApp/signup.html')
