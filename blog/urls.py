60415d3ba7f24348e0f07690e4113081f298b613
Tarek Eldebsy <TarekEldebsy94>
2022-02-14 04:12:33 +0200

    adding views linked to categories

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
-  path('',views.navbar ,name = 'nav'),
-  path('home/',views.home , name = 'home'),
-    path('homepage/',views.homepage , name = 'homepage'),
+  
+  path('homepage/',views.homepage , name = 'homepage'),
   path('register/',views.register , name = 'register'),
   path('login/',views.userlogin , name = 'login'),
-  path('logout/',views.userlogout , name = 'logout')
+  path('logout/',views.userlogout , name = 'logout'),
+  # categories
+  path('det_category/<int:id>',views.det_category, name='det_category'),
+  path('subscribe/<int:id>',views.subscribe, name='subscribe'),
+  path('user_subscriptions',views.user_subscriptions, name='user_subscriptions')
 ]
 
